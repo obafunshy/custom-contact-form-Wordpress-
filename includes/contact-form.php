0c9cd23e188164ea494aab5d08b213dce107621f
Oluwaseun F <pharma4mee@yahoo.com>
2024-07-24 19:15:25 +0100

    cleaned up codes

--- includes/contact-form.php
+++ includes/contact-form.php
@@ -1,1 +1,121 @@
 <?php 
+
+ // add shortcode
+add_shortcode('contact', 'show_contact_form');
+
+// add_action
+add_action('rest_api_init', 'create_rest_endpoint');
+
+function show_contact_form() {
+    include MY_PLUGIN_PATH . 'includes/templates/contact-form.php';
+}
+
+function create_rest_endpoint() {
+    register_rest_route('v1/contact-form', 'submit', array(
+        'methods' => 'POST',
+        'callback' => 'handle_enquiry'
+    ));
+}
+
+function handle_enquiry($data) {
+    // Handle the form data that is posted
+
+      // Get all parameters from form
+      $params = $data->get_params();
+
+      // Set fields from the form
+      $field_name = sanitize_text_field($params['name']);
+      $field_email = sanitize_email($params['email']);
+      $field_phone = sanitize_text_field($params['phone']);
+      $field_message = sanitize_textarea_field($params['message']);
+
+
+      // Check if nonce is valid, if not, respond back with error
+      if (!wp_verify_nonce($params['_wpnonce'], 'wp_rest')) {
+
+            return new WP_Rest_Response('Message not sent', 422);
+      }
+
+      // Remove unneeded data from paramaters
+      unset($params['_wpnonce']);
+      unset($params['_wp_http_referer']);
+
+      // Send the email message
+      $headers = [];
+
+      $admin_email = get_bloginfo('admin_email');
+      $admin_name = get_bloginfo('name');
+
+      // Set recipient email
+      $recipient_email = get_plugin_options('contact_plugin_recipients');
+
+      if (!$recipient_email) {
+            // Make all lower case and trim out white space
+            $recipient_email = strtolower(trim($recipient_email));
+      } else {
+
+            // Set admin email as recipient email if no option has been set
+            $recipient_email = $admin_email;
+      }
+
+
+      $headers[] = "From: {$admin_name} <{$admin_email}>";
+      $headers[] = "Reply-to: {$field_name} <{$field_email}>";
+      $headers[] = "Content-Type: text/html";
+
+      $subject = "New enquiry from {$field_name}";
+
+      $message = '';
+      $message = "<h1>Message has been sent from {$field_name}</h1>";
+
+
+      $postarr = [
+
+            'post_title' => $params['name'],
+            'post_type' => 'submission',
+            'post_status' => 'publish'
+
+      ];
+
+      $post_id = wp_insert_post($postarr);
+
+      // Loop through each field posted and sanitize it
+      foreach ($params as $label => $value) {
+
+            switch ($label) {
+
+                  case 'message':
+
+                        $value = sanitize_textarea_field($value);
+                        break;
+
+                  case 'email':
+
+                        $value = sanitize_email($value);
+                        break;
+
+                  default:
+
+                        $value = sanitize_text_field($value);
+            }
+
+            add_post_meta($post_id, sanitize_text_field($label), $value);
+
+            $message .= '<strong>' . sanitize_text_field(ucfirst($label)) . ':</strong> ' . $value . '<br />';
+      }
+
+
+      wp_mail($recipient_email, $subject, $message, $headers);
+
+      $confirmation_message = "The message was sent successfully!!";
+
+      if (get_plugin_options('contact_plugin_message')) {
+
+            $confirmation_message = get_plugin_options('contact_plugin_message');
+
+            $confirmation_message = str_replace('{name}', $field_name, $confirmation_message);
+      }
+
+      return new WP_Rest_Response($confirmation_message, 200);
+}
+    
--- simple-contact-form.php
+++ simple-contact-form.php
@@ -1,11 +1,11 @@
 <?php
 /** 
- * Plugin name: Simple Contact Form
+ * Plugin name: Contact plugin
  * Description: Simple contact form series
  * Author: Seun Familusi
  * Author URI: https://cowebplus.com
  * Version: 1.0.0
- * Text Domain: simple-contact-form
+ * Text Domain: contact-plugin
  * 
  */
 
@@ -45,174 +45,10 @@ if(!class_exists('ContactPlugin')){
             include_once( MY_PLUGIN_PATH . '/includes/utilities.php' );
             include_once( MY_PLUGIN_PATH . '/includes/options-page.php' );
             include_once( MY_PLUGIN_PATH . '/includes/contact-form.php' );
+            
         }
-    
-//         public function create_custom_post_type() {
-            
-//             $args = array(
-//                 'public' => true,
-//                 'has_archive' => true,
-//                 'supports' => array('title'),
-//                 'exclude_from_search' => true,
-//                 'publicly_queryable' => false,
-//                 'capability' => 'manage_options',
-//                 'labels' => array(
-//                     'name' => 'Contact Form',
-//                     'singular_name' => 'Contact Form Entry'
-//                 ),
-//                 'menu_icon' => 'dashicons-media-text',
-    
-//             );
-    
-//             register_post_type('simple_contact_form', $args);
-//         }
-    
-//     //     public function load_assets() {
-//     //         wp_enqueue_style(
-//     //             'simple-contact-form',
-//     //             plugin_dir_url( __FILE__) . 'css/simple-contact-form.css',
-//     //             array(),
-//     //             1,
-//     //             'all'
-//     //         );
-    
-//     //         wp_enqueue_script(
-//     //             'simple-contact-form-react',
-//     //             plugin_dir_url( __FILE__) . 'js/simple-contact-form-react.js',
-//     //             array('wp-element'),
-//     //             1,
-//     //             true
-//     //         );
-//     // // what does this do?
-//     //         wp_localize_script('simple-contact-form-react', 'simpleContactFormSettings', array(
-//     //             'restURL' => get_rest_url(null, 'simple-contact-form/v1/send-email'),
-//     //             'nonce' => wp_create_nonce('wp_rest')
-//     //         ));
-//     //     }
-    
-//     public function load_assets() {
-//         wp_enqueue_style(
-//             'simple-contact-form',
-//             plugin_dir_url(__FILE__) . 'css/simple-contact-form.css',
-//             array(),
-//             1,
-//             'all'
-//         );
-    
-//         // Enqueue React and ReactDOM from CDN
-//         wp_enqueue_script('react', 'https://unpkg.com/react@18/umd/react.development.js', array(), null, true);
-//         wp_enqueue_script('react-dom', 'https://unpkg.com/react-dom@18/umd/react-dom.development.js', array('react'), null, true);
-    
-//         wp_enqueue_script(
-//             'simple-contact-form-react',
-//             plugin_dir_url(__FILE__) . 'js/simple-contact-form-react.js',
-//             array('react', 'react-dom'),
-//             1,
-//             true
-//         );
-    
-//         wp_localize_script('simple-contact-form-react', 'simpleContactFormSettings', array(
-//             'restURL' => get_rest_url(null, 'simple-contact-form/v1/send-email'),
-//             'nonce' => wp_create_nonce('wp_rest')
-//         ));
-//     }
-    
-    
-//         // public function load_shortcode() {
-    
-//          //     <div class="simple-contact-form">
-//         //             <h1>Send us an email</h1>
-//         //             <p>Please fill the below form</p>
-    
-//         //             <form action="" id="simple-contact-form__form">
-//         //                 <div class="form-group mb-2">
-//         //                     <input type="text" name="name" class="form-control" placeholder="Name" >
-//         //                 </div>
-//         //                 <div class="form-group mb-2">
-//         //                     <input type="email" name="email"  class="form-control" placeholder="Email" >
-//         //                 </div>
-//         //                 <div class="form-group mb-2">
-//         //                     <input type="tel" name="phone"  class="form-control" placeholder="Phone" >
-//         //                 </div>
-//         //                 <div class="form-group mb-2"> 
-//         //                     <textarea name="message" class="form-control" id="" placeholder="Type your message"></textarea> 
-//         //                 </div>
-//         //                 <div class="form-group mb-2">
-//         //                     <button type="submit" class="btn btn-success btn-block w-100">Send Message</button>
-//         //                 </div>
-//         //             </form>
-//         //     </div>
-//         // <?php -->
-        
-//         // }  
-    
-//         public function load_shortcode() {
-//             echo '<div id="simple-contact-form-root"></div>';
-//         }
-    
-//         public function load_scripts() 
-//             <script>
-//                 var nonce = '<?php echo wp_create_nonce('wp_rest') ';
-    
-//                 (function($){
-//                     $('#simple-contact-form__form').submit( function(event) {
-//                     event.preventDefault();
-//                     // alert('submitted');
-//                     // serializes the form name inputs
-//                     var form = $(this).serialize();
-//                     console.log(form);
-                    
-//                     $.ajax({
-//                         method: 'post',
-//                         url: '<?php echo get_rest_url(null, 'simple-contact-form/v1/send-email'); ',
-//                         headers: {'X-WP-Nonce': nonce},
-//                         data: form
-    
-//                     })
-//                 });
-//                 })(jQuery)
-//             </script>
-            
-//         <?php
-// }
 
-    
-//     }  
-
-//     public function register_rest_api() {
-//         register_rest_route('simple-contact-form/v1', 'send-email', array(
-//             'methods' => 'POST',
-//             'callback' => array($this, 'handle_contact_form')
-//         ));
-//     }
-
-//     public function handle_contact_form($data) {
-//         $headers = $data->get_headers();
-//         $params = $data->get_params();
-//         // test if the nonce is on console tools
-//         // echo json_encode($headers);
-//         $nonce = $headers['x_wp_nonce'][0];
-//         // $nonce = 55656666;
-
-//         if(!wp_verify_nonce($nonce, 'wp_rest')) {
-//             // echo 'This nonce is correct';
-//             return new WP_REST_Response('Unauthorized request', 401);
-//         }
-
-//         $post_id = wp_insert_post([
-//             'post_type' => 'simple_contact_form',
-//             'post_title' => 'Contact enquiry',
-//             'post_status' => 'publish',
-//         ]);
-
-//         if($post_id) {
-//             return new WP_REST_Response('Thank you for your email', 200);
-//         }
-
-//         return new WP_REST_Response('Message not sent', 500);
-
-//     }
-            }
+    }
         $contactPlugin = new ContactPlugin();
 
         $contactPlugin->initialize();
