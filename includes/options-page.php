8d41741ee079428fbc5bafef8a355a77457b5d76
Oluwaseun F <pharma4mee@yahoo.com>
2024-07-24 10:43:23 +0100

    carbon removed

--- includes/options-page.php
+++ includes/options-page.php
@@ -1,3 +1,10 @@
 <?php 
+use Carbon_Fields\Container;
+use Carbon_Fields\Field;
 
 
+add_action('after_setup_theme', 'load_carbon_fields');
+
+function load_carbon_fields() {
+    \Carbon_Fields\Carbon_Fields::boot();
+}
